8845cbaa767b8950106b1a770f77ba92f0d784be
Anoop <anooppassi66@gmail.com>
2026-05-12 18:06:18 +0530

    changes

--- app/join-now/page.tsx
+++ app/join-now/page.tsx
@@ -146,7 +146,6 @@ export default function JoinNowPage() {
               alt="Neurocruit"
               width={190}
               height={76}
-              className="brightness-0 invert"
               priority
             />
           </div>
